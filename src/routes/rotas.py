0116f7f7d1727163bad069eb74d38f67c2fd5b67
João Paulo <joaomorais832@gmail.com>
2023-08-30 21:48:42 -0300

    2.5

--- src/routes/rotas.py
+++ src/routes/rotas.py
@@ -4,4 +4,4 @@ from src.controllers.jogo_controller import *
 
 app.add_url_rule('/', view_func=carregarIndex.as_view('index'))
 app.add_url_rule('/listarRotas', view_func=listarRotas.as_view('listar_rotas'))
-app.add_url_rule('/escolher', view_func=escolher.as_view('escokher'))
+app.add_url_rule('/escolher', view_func=escolher.as_view('escolher'))
