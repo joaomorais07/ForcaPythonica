0728e6426feca8b39c69ab1b01163fff51af782a
João Paulo <joaomorais832@gmail.com>
2024-02-07 21:38:41 -0300

    Volta da /

--- src/routes/rotas.py
+++ src/routes/rotas.py
@@ -2,6 +2,6 @@
 from src.controllers.rota_controller import *
 from src.controllers.jogo_controller import *
 
-app.add_url_rule('/jogo', view_func=carregarJogo.as_view('jogo'))
+app.add_url_rule('/', view_func=carregarJogo.as_view('jogo'))
 app.add_url_rule('/listarRotas', view_func=listarRotas.as_view('listar_rotas'))
 app.add_url_rule('/escolher', view_func=escolher.as_view('escolher'))
